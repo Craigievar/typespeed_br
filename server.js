4e2089ccd57fb6f553b97216a0579f045d604aba
Brent Echols <brentechols@fb.com>
2019-07-17 20:09:22 -0700

    prettier everything

--- server.js
+++ server.js
@@ -39,23 +39,21 @@ server.listen(process.env.PORT || 5000, function() {
 });
 
 // Add the WebSocket handlers
-io.on('connection', function(socket) {
-});
+io.on('connection', function(socket) {});
 
 // Initialize the game state when we start the server
 const gameState = {
-  players : {},
-  state : 'LOBBY', // LOBBY, INGAME
-  playersLeft : 0,
+  players: {},
+  state: 'LOBBY', // LOBBY, INGAME
+  playersLeft: 0,
   loadTime: COUNTDOWN_LENGTH,
   inCountdown: false,
   delay: MS_PER_WORD_BASE,
   playersNeeded: MIN_PLAYERS_TO_START,
   time: 0,
   endTime: 0,
-  winner: ''
+  winner: '',
 };
-
 
 function randomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -74,32 +72,37 @@ function checkIfLost(player) {
 
 function newPlayer(socket) {
   return {
-      prevWords: [],
-      nextWords: [],
-      lost: false,
-      won: false,
-      //target: findTarget(players, socket.id),
-      lobbyIndex: gameState.players.length, //TODO; DYNAMICALLY FETCH THIS
-      inGame: false,
-      ready: false,
-      name: '',
-      kills: 0,
-      wrongAnswers: 0,
-      rightAnswers: 0,
-      lastAttacker: '',
-      winner: '',
-      deathTime: 1
-    };
+    prevWords: [],
+    nextWords: [],
+    lost: false,
+    won: false,
+    //target: findTarget(players, socket.id),
+    lobbyIndex: gameState.players.length, //TODO; DYNAMICALLY FETCH THIS
+    inGame: false,
+    ready: false,
+    name: '',
+    kills: 0,
+    wrongAnswers: 0,
+    rightAnswers: 0,
+    lastAttacker: '',
+    winner: '',
+    deathTime: 1,
+  };
 }
 
 function findTarget(players, player) {
   const playerList = Object.keys(players);
-  const filtered = playerList.filter(function(value, index, arr){
-    return value !== player && players[value].inGame && !players[value].won && !players[value].lost;
+  const filtered = playerList.filter(function(value, index, arr) {
+    return (
+      value !== player &&
+      players[value].inGame &&
+      !players[value].won &&
+      !players[value].lost
+    );
   });
   if (filtered.length > 0) {
     const randomId = randomElement(filtered);
-    if(randomId) {
+    if (randomId) {
       return randomId;
     }
   }
@@ -133,16 +136,16 @@ function numReadyPlayers(game) {
 }
 
 function checkForWinner(game) {
-  if(updatePlayersLeft(game) <= PLAYERS_TO_WIN) {
-    console.log("Reset game");
+  if (updatePlayersLeft(game) <= PLAYERS_TO_WIN) {
+    console.log('Reset game');
     game.loadTime = RESET_LENGTH;
-    console.log("reset length is " + RESET_LENGTH);
+    console.log('reset length is ' + RESET_LENGTH);
     game.state = 'POSTGAME';
     if (game.endTime === 0) {
       game.endTime = game.time;
     }
     for (const player of game.players) {
-      if (player && !player.lost){
+      if (player && !player.lost) {
         player.won = true;
         game.winner = player.id;
       }
@@ -150,21 +153,20 @@ function checkForWinner(game) {
   }
 }
 
-function checkInput(word, player, id){
-  if(!word){
+function checkInput(word, player, id) {
+  if (!word) {
     return;
   }
 
-  if(word.toLowerCase() === player.nextWords[0].toLowerCase()){
+  if (word.toLowerCase() === player.nextWords[0].toLowerCase()) {
     player.rightAnswers++;
     player.nextWords.shift();
     const target = findTarget(gameState.players, id);
-    if(gameState.players[target]){
+    if (gameState.players[target]) {
       gameState.players[target].nextWords.push(word);
       gameState.players[target].lastAttacker = id;
     }
-  }
-  else {
+  } else {
     player.wrongAnswers++;
     player.nextWords.shift();
     player.nextWords.push(randomWord());
@@ -173,15 +175,13 @@ function checkInput(word, player, id){
   player.prevWords.push(word);
 }
 
-const generateWords = function (){
+const generateWords = function() {
   //generate words
   for (const player of gameState.players) {
-    if (player && gameState.state === 'INGAME' &&
-      !player.won && !player.lost) {
+    if (player && gameState.state === 'INGAME' && !player.won && !player.lost) {
       player.nextWords.push(randomWord());
     }
   }
-
 
   if (gameState.delay > MS_PER_WORD_MIN) {
     gameState.delay -= MS_PER_WORD_DELTA;
@@ -193,7 +193,7 @@ const generateWords = function (){
 };
 
 function resetGame(game) {
-  console.log("Updating game state");
+  console.log('Updating game state');
   game.state = 'LOBBY';
   game.playersLeft = 0;
   game.loadTime = COUNTDOWN_LENGTH;
@@ -201,7 +201,7 @@ function resetGame(game) {
   game.delay = MS_PER_WORD_BASE;
   game.playersNeeded = MIN_PLAYERS_TO_START;
 
-  for (const {name, id} of game.players){
+  for (const { name, id } of game.players) {
     game.players[id] = newPlayer();
     game.players[id].name = name;
 
@@ -217,7 +217,7 @@ function updateGameState(game) {
     game.playersNeeded = MIN_PLAYERS_TO_START - playersReady;
     if (playersReady >= MIN_PLAYERS_TO_START) {
       //start game!
-      console.log("starting game");
+      console.log('starting game');
       game.state = 'INGAME';
       game.inCountdown = true;
 
@@ -225,9 +225,7 @@ function updateGameState(game) {
         player.inGame = true;
       }
     }
-  }
-
-  else if (game.state === 'INGAME') {
+  } else if (game.state === 'INGAME') {
     if (game.loadTime >= 0) {
       game.loadTime -= 1000 / 60;
     } else {
@@ -242,7 +240,7 @@ function updateGameState(game) {
       for (const player of game.players) {
         const hadLost = player.lost;
         player.lost = checkIfLost(player);
-        if(player.lost){
+        if (player.lost) {
           player.deathTime = game.time;
           const killer = player.lastAttacker;
           if (!hadLost && game.players[killer]) {
@@ -253,13 +251,10 @@ function updateGameState(game) {
       game.playersLeft = updatePlayersLeft(game);
       checkForWinner(game);
     }
-  }
-
-  else if (game.state === 'POSTGAME') {
+  } else if (game.state === 'POSTGAME') {
     if (game.loadTime >= 0) {
       game.loadTime -= 1000 / 60;
-    }
-    else {
+    } else {
       console.log(game.state);
       console.log(game.loadTime);
       resetGame(game);
@@ -286,7 +281,7 @@ io.on('connection', function(socket) {
   });
 
   socket.on('start', function(data) {
-    console.log("starting game");
+    console.log('starting game');
     gameState.state = 'INGAME';
     gameState.inCountdown = true;
     for (const player of gameState.players) {
@@ -298,7 +293,7 @@ io.on('connection', function(socket) {
     if (gameState.state === 'INGAME') {
       const player = gameState.players[socket.id] || {};
       checkInput(data.word, player, socket.id);
-      }
+    }
   });
 });
 
