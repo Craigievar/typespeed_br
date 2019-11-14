444d47c8f918909eefb660f55e709ec1b2eca9e0
Brent Echols <brentechols@fb.com>
2019-11-13 23:13:25 -0800

    added postgame info

--- src/PostGameView.js
+++ src/PostGameView.js
@@ -1,11 +1,53 @@
 // @flow
 
+import type GameNetwork from './network/GameNetwork';
+import type { GameState } from './gameTypes';
+
 import React from 'react';
 
-function PostGameView() {
+type Props = {
+  gameServer: GameNetwork,
+  gameState: GameState,
+};
+
+function PostGameView({ gameServer, gameState }: Props) {
+  const player = gameState.players[gameServer.getSocketID()];
+
   return (
     <div>
-      Post Game...
+      {player && !player.inGame && (
+        <>
+          <div>{'In game with ' + gameState.playersLeft + ' players left'}</div>
+          <div>{'Next Game Starts In ' + Math.floor(gameState.loadTime / 1000)}</div>
+        </>
+      )}
+      {player && player.lost && (
+        <>
+          {player.lastAttacker && player.lastAttacker.length > 0 && (<div>{'Killed by ' + gameState.players[player.lastAttacker].name}</div>)}
+          {!(player.lastAttacker && player.lastAttacker.length > 0) && (<div>{'You Lose :('}</div>)}
+        </>
+      )}
+      {player && player.won && (
+          <div>You Won!</div>
+      )}
+      <div>
+        {Math.round(player.rightAnswers / (player.deathTime / 1000.0 / 60)) +
+          ' WPM'}
+      </div>
+      <div>
+        {'Your accuracy: ' +
+          Math.round(
+            (player.rightAnswers / (player.rightAnswers + player.wrongAnswers)) *
+              100
+          ) +
+          '%'}
+      </div>
+      <div>
+        {"You KO'd " + player.kills + ' players'}
+      </div>
+      <div>
+        {'Next Game Starts In ' + Math.floor(gameState.loadTime / 1000)}
+      </div>
     </div>
   );
 }
