ce9aa251eded10b2707cf5fe856fb88065d60b7b
Craigievar <craigsexauer@gmail.com>
2019-12-24 21:12:56 -0800

    hmm

--- test_k8s_setup.sh
+++ test_k8s_setup.sh
@@ -1,3 +1,9 @@
+kubectl label nodes gke-hello-cluster-default-pool-184e5807-6xt7 role=game-instance-manager-server
+kubectl label nodes gke-hello-cluster-default-pool-184e5807-937p role=matchmaker
+kubectl label nodes gke-hello-cluster-pool-1-c35d7dd9-0zcz role=game-assets-server
+kubectl label nodes gke-hello-cluster-pool-1-c35d7dd9-pnqc role=game-server
+
+
 kubectl apply -f game/service.yaml
 kubectl apply -f game/pod.yaml
 
