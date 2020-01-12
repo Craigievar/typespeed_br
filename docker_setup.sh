b66fe200c5fd7f201d718fb71c4a49e9687abbe1
Craigievar <craigsexauer@gmail.com>
2020-01-11 22:07:41 -0800

    lol

--- docker_setup.sh
+++ docker_setup.sh
@@ -2,8 +2,10 @@ docker build game -t gcr.io/tsbr-cluster-demo/game:v0
 docker build game_instance_manager -t gcr.io/tsbr-cluster-demo/game-instance-manager:v0
 docker build game_server -t gcr.io/tsbr-cluster-demo/game-server:v0
 docker build matchmaker -t gcr.io/tsbr-cluster-demo/matchmaker:v0
+docker build api -t gcr.io/tsbr-cluster-demo/api:v0
 
 docker push gcr.io/tsbr-cluster-demo/game:v0
 docker push gcr.io/tsbr-cluster-demo/game-instance-manager:v0
 docker push gcr.io/tsbr-cluster-demo/game-server:v0
 docker push gcr.io/tsbr-cluster-demo/matchmaker:v0
+docker push gcr.io/tsbr-cluster-demo/api:v0
