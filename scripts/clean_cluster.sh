cc32f5323a4104d704273019eefb5240f1d07ffe
craigievar <csexauer@fb.com>
2020-01-14 11:47:44 -0800

    scripts folder

---
+++ scripts/clean_cluster.sh
@@ -0,0 +1,3 @@
+kubectl delete pods --all
+kubectl delete deployments --all
+kubectl delete services --all
--- scripts/setup_cluster.sh
+++ scripts/setup_cluster.sh
@@ -4,6 +4,11 @@ gcloud config set project tsbr-cluster-demo
 # firewall permissions
 gcloud compute firewall-rules create my-rule --allow=tcp:7000-8000
 
+kubectl label nodes gke-hello-cluster-default-pool-184e5807-6xt7 role=apps --overwrite=True
+kubectl label nodes gke-hello-cluster-default-pool-184e5807-937p role=apps --overwrite=True
+kubectl label nodes gke-hello-cluster-pool-1-c35d7dd9-0zcz role=apps --overwrite=True
+kubectl label nodes gke-hello-cluster-pool-1-c35d7dd9-pnqc role=game-server --overwrite=True
+
 # make default service accounts admins for kube api
 # should clean this up later!!!
 kubectl apply -f api/rbac.yaml
